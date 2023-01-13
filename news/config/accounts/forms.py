8f9a8f28756a09f98542d889b65a30d67c74aec1
Ehasan Ahmed <ahmed.ehsan1258@gmail.com>
2023-01-13 22:25:53 +0600

    Initial Commit

---
+++ news/config/accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class meta(UserCreationForm):
+        models = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age', )
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta: 
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
