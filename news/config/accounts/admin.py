b675fccb7f945aabb90ce81dfa5c3067a0243886
Ehasan Ahmed <ahmed.ehsan1258@gmail.com>
2023-01-14 01:22:32 +0600

    fixes

--- news/config/accounts/admin.py
+++ news/config/accounts/admin.py
@@ -7,9 +7,10 @@ from .models import CustomUser
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = CustomUserChangeForm
+    model = CustomUser
     list_display = ['email', 'username', 'age', 'is_staff', ]
-    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age', )}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('age')}), )
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('age',)}),)
 
-admin.site.register(CustomUser, CustomUserAdmin)
+
+admin.site.register(CustomUser, CustomUserAdmin, )
